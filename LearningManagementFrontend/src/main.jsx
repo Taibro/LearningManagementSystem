2204fe2e90f668c8dba043512972bea1dc69447a
Tai <ntai8448@gmail.com>
2026-05-26 08:30:51 +0700

    add src

--- LearningManagementFrontend/src/main.jsx
+++ LearningManagementFrontend/src/main.jsx
@@ -7,8 +7,8 @@ import AppStudent from './StudentPages/AppStudent.jsx'
 import AppAdmin from './SchoolAdmin/AppAdmin.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <AppLecturer /> */}
+    <AppLecturer />
     {/* <AppStudent/> */}
-    <AppAdmin/>
+    {/* <AppAdmin/> */}
   </React.StrictMode>,
 )
